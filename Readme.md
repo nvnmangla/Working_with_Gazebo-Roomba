d738d2e8ec3454cfad9e985e0672e93b444c19ac
Naveen Mangla <nmangla@umd.edu>
2022-12-05 11:45:32 -0500

    Readme,cpplint,cppcheck

---
+++ Readme.md
@@ -0,0 +1,78 @@
+# roomba_ros2
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+---
+# Working_with_Gazebo-Roomba
+
+### Overview
+
+This is a ROS Package that defines a basic obstacle_avoidance using Gazebo and Turtlebot3.
+
+### Dependencies/ Assumptions
+- OS : Ubuntu 20.04 
+- ROS2 Distro : ROS2 Galactic
+- Package build type : ```ament_cmake ```
+- Package dependencies : ```rclcpp```, ```std_msgs``` ,```turtlebot3_gazebo```
+- ROS2 Galactic Installation : [link](https://docs.ros.org/en/galactic/Installation/Ubuntu-Install-Debians.html)
+
+## How to Run the ROS Package
+### Build Instructions
+```
+cd <your_ROS2_ws>/src
+git clone https://github.com/nvnmangla/Working_with_Gazebo-Roomba.git
+cd ..   
+rosdep install -i --from-path src --rosdistro galactic -y
+colcon build --packages-select roomba
+source . install/setup.bash
+source ~/<your ROS2 installation>/opt/ros/galactic/setup.bash
+```
+
+### Run Publisher
+To run the publisher node, open a new terminal and run:
+```
+cd <your_ROS2_ws>
+. install/setup.bash
+ros2 run roomba walk
+```
+Make sure you have  installed turtlebot3 and you are not i conda enviornment 
+
+### Launch File
+To launch nodes using launch file and pass parameters using command line:
+```
+cd <ROS2_ws>/src/roomba/launch
+```
+If you want to launch with bag recording on
+```
+ros2 launch launch_roomba.yaml ros_record:=true 
+```
+```
+ros2 launch launch_roomba.yaml ros_record:=false 
+```
+otherwise
+### ROS2 Bags
+Bag files are available in
+```
+<ros_ws>/src/roomba/results/bag
+```
+## Results
+The results after running the following commands are stored in the <your_package>/results folder.
+
+### rqt Console
+```
+ ros2 run rqt_console rqt_console
+
+```
+### cppcheck
+Run the following command from the root directory of your ROS package
+```
+cppcheck --enable=all --std=c++17 ./src/*.cpp ./include/*.hpp --suppress=missingIncludeSystem --suppress=unmatchedSuppression --suppress=unusedFunction --suppress=missingInclude --suppress=useInitializationList > results/cppcheck.txt
+```
+### cpplint
+Run the following command from the root directory of your ROS package
+```
+ cpplint --filter=-build/c++11,+build/c++17,-build/namespaces,-build/include_order ./src/*.cpp ./include/*.hpp > ./results/cpplint.txt
+```
+### Google Styling format
+This command will autstyle the files.
+```
+clang-format -style=Google -i src/*.cpp include/*.hpp
+```
---
+++ include/roomba.hpp
@@ -0,0 +1,149 @@
+/*
+Copyright 2022 Naveen Mangla
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/**
+ * @file roomba.hpp
+ * @author Naveen Mangla (nmangla@umd.edu)
+ * @brief Roomba Walker, avoid obstacles if any,class definition
+ * @version 0.1
+ * @date 2022-12-05
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#ifndef ROS2_WS_SRC_ROOMBA_INCLUDE_ROOMBA_HPP_
+#define ROS2_WS_SRC_ROOMBA_INCLUDE_ROOMBA_HPP_
+#include <geometry_msgs/msg/twist.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp>
+
+using std::placeholders::_1;
+using namespace std::chrono_literals;
+
+using LASER = sensor_msgs::msg::LaserScan;
+using TWIST = geometry_msgs::msg::Twist;
+
+// Bot state
+typedef enum {
+  FORWARD = 0,
+  STOP,
+  TURN,
+} StateType;
+
+/**
+ * @brief Classs definination for RoomBa
+ *
+ */
+class RoomBa : public rclcpp::Node {
+ public:
+  RoomBa() : Node("walker"), state_(FORWARD) {
+    auto pubTopicName = "cmd_vel";
+    publisher_ = this->create_publisher<TWIST>(pubTopicName, 10);
+
+    // Publisher callback
+    auto processCallback = std::bind(&RoomBa::callback, this);
+    timer_ = this->create_wall_timer(100ms, processCallback);
+
+    auto subCallback = std::bind(&RoomBa::subscribe_callback, this, _1);
+    auto default_qos = rclcpp::QoS(rclcpp::SensorDataQoS());
+    subscription_ = this->create_subscription<LASER>(
+        "scan", default_qos, subCallback);  // subscribing  to topic scan
+  }
+
+ private:
+  /**
+   * @brief Publisher callback function
+   *
+   */
+  void callback() {
+    auto message = TWIST();
+
+    if (laser_.header.stamp.sec == 0) {
+      message.linear.x = 0.3;
+      publisher_->publish(message);
+      return;
+    }
+    // state machine (Mealy -- output on transition)
+    switch (state_) {
+      case FORWARD:
+        message.linear.x = 0.3;
+        RCLCPP_INFO(this->get_logger(), "Moving forward");
+
+        if (hasObstacle()) {  // check transition
+          state_ = STOP;
+          publisher_->publish(message);
+          RCLCPP_INFO_STREAM(this->get_logger(), "State = STOP");
+        }
+        break;
+      case STOP:
+        RCLCPP_INFO(this->get_logger(), "Stopped");
+
+        if (hasObstacle()) {  // check transition
+          state_ = TURN;
+          // message.linear.x = 0;
+          message.angular.z = 0.3;
+          publisher_->publish(message);
+          RCLCPP_INFO_STREAM(this->get_logger(), "State = TURN");
+
+        } else {
+          state_ = FORWARD;
+          message.linear.x = 0.3;
+          publisher_->publish(message);
+          RCLCPP_INFO_STREAM(this->get_logger(), "State = FORWARD");
+        }
+        break;
+      case TURN:
+        RCLCPP_INFO(this->get_logger(), "Turning");
+        if (!hasObstacle()) {  // check transition
+          state_ = FORWARD;
+          message.linear.x = 0.3;
+          publisher_->publish(message);
+          RCLCPP_INFO_STREAM(this->get_logger(), "State = FORWARD");
+        }
+        break;
+    }
+
+    /**
+     * @brief Subsciber callback
+     *
+     */
+  }
+  void subscribe_callback(const LASER& msg) { laser_ = msg; }
+  /**
+   * @brief Checking whether obstacle is present
+   *
+   * @return true
+   * @return false
+   */
+  bool hasObstacle() {
+    for (int i{}; i < num_readings; i++) {
+      // Here 1 is range to check
+      if (laser_.ranges[i] < 1 || (laser_.ranges[359 - i] < 1)) {
+        auto dist = (laser_.ranges[i] + laser_.ranges[359 - i]) / 2;
+        RCLCPP_INFO(this->get_logger(), "Obstacle at  '%f'm ", dist);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  rclcpp::Subscription<LASER>::SharedPtr subscription_;
+  rclcpp::Publisher<TWIST>::SharedPtr publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  LASER laser_;           // laser message
+  StateType state_;       // state
+  int num_readings = 15;  // angle to check, -15 to 15
+};
+#endif  // ROS2_WS_SRC_ROOMBA_INCLUDE_ROOMBA_HPP_
